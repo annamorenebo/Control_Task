44726bd609abbfff8d2328316a75f79b2c9e8b62
Anna <anna@rumyantseva.org>
2023-01-30 22:23:01 +0300

    ReadData and Print1DArr methods added

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+//метод считывает данные 
+string? ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return Console.ReadLine();
+}
+//метод печатает массив
+void Print1DArr(string[] arr)
+     {   Console.Write("[ ");
+         for (int i = 0; i < arr.Length-1; i++)
+         {  
+            
+             Console.Write(arr[i]+" , ");
+         }
+         Console.WriteLine( arr[arr.Length - 1]+ "]");
+     } 
+
+
+
+
+
+
+
