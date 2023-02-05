810db80285c16a92ea68fa06f87d7a76c0121fdc
Anna <anna@rumyantseva.org>
2023-02-05 19:08:14 +0300

    solution added

--- Program.cs
+++ Program.cs
@@ -16,7 +16,31 @@ void Print1DArr(string[] arr)
          Console.WriteLine( arr[arr.Length - 1]+ "]");
      } 
 
+// разделяет строку на элементы массива c помощью функции Split 
+//и формиру итоговый массив.
+string[] OutStringArr(string inpString)
+{
+    string[] arr = inpString.Split(",");
+    int count=0;
+    int j=0;
+    string[] OutArr=new string[arr.Length];
+    for(int i=0; i<arr.Length; i++)
+    {
+        if(arr[i].Length<=3)
+        {
+            OutArr[j++]=arr[i];
+            count++;
+        }
 
+    }
+    Array.Resize(ref OutArr,count);
+    return OutArr;
+}
+
+
+
+string inpString = ReadData("Введите строки через запятую: ");
+Print1DArr(OutStringArr(inpString));
 
 
 
